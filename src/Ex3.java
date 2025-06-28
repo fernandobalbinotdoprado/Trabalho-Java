ab2a1b0542f4cd77a8dd15b194ce4fb58b2a945a
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-06-28 20:18:21 -0300

    Ex3

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,6 +1,6 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
+// .
 public class Ex3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
